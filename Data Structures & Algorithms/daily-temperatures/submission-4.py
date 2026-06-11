bd367c1619724267a03e3a39457b1e0f13d5051b
Giovanny Ecinia <61249454+Giovanny-Encinia@users.noreply.github.com>
2026-06-10 19:10:09 -0600

    Add: daily-temperatures - submission-4

---
+++ Data Structures & Algorithms/daily-temperatures/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = []
+        for i, t in enumerate(temperatures):
+            count = 0
+            for y in temperatures[i + 1:]:
+                print(t, y)
+                if y > t:
+                    count+=1
+                    break
+                else:
+                    count += 1
+                
+            else:
+                count = 0
+                    
+            result.append(count)
+        return result
