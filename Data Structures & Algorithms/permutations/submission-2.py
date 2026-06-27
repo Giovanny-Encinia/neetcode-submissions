ffada0df5b41b35c29fd2eade9eae5cd44c70601
Giovanny Ecinia <61249454+Giovanny-Encinia@users.noreply.github.com>
2026-06-27 16:41:59 -0600

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        def dfs(nums, current_perm, result):
+
+            if len(current_perm) == len(nums):
+                result.append(current_perm[:])
+                return None
+            
+            for i in range(len(nums)):
+                n = nums[i]
+                if n not in current_perm:
+                    current_perm.append(n)
+                    dfs(nums, current_perm, result)
+                    current_perm.pop()
+            # 0, 3
+            """
+            0, 3
+            current_perm [1]
+            1, 3
+            current_perm [1, 2]
+            2, 3
+            current_perm [1, 2, 3]
+            return result [[1, 2, 3]]
+
+            current_perm [1, 2]
+            """
+
+        current_perm = []
+        result = []
+        dfs(nums, current_perm, result)
+        return result
